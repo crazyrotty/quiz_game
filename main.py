38ea9a56569ac11323522f5dc4c722a423f4be02
Kenneth Scribner <kennethscribner@yahoo.com>
2025-03-02 16:33:35 -0700

    getting the question to loop

--- main.py
+++ main.py
@@ -12,4 +12,6 @@ for question in question_data:
 
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():#If the gamer has another question
+    quiz.next_question()
